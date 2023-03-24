4f30aeb1fc7588c710ceb110942846935e73f204
Alejandro Guerrero <guerreroe200@yahoo.com>
2023-03-24 11:27:10 -0600

    Avanze, se tienen shaders

--- main.cpp
+++ main.cpp
@@ -6,9 +6,27 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);//Funcion para manejar el resize
 void processInput(GLFWwindow* window); //PRocesa los inputs, va en render loop
 
+//REAally simple basic vertex shader
+    const char *vertexShaderSource = "#version 330 core\n" 
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n" 
+    "}\0";
+//------------------------
+
+//REAally simple basic fragment shader
+    const char *fragmentShaderSource = "#version 330 core\n" 
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    " FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n" 
+    "}\0";
+//------------------------
 
 int main(void)
-{
+{   
+    // Initialize opengl -----------------------------------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //opengl version max
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //opengl version minimum
@@ -33,7 +51,60 @@ int main(void)
 
     glViewport(0,0,800,600); //PAra ver lo que rederizemos?
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);//decirle a opengl que use my funcion de resize cuando suceda
+    //-------------------------------------------------------------------------------------------
+
+    //doin a triangle
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    }; //vertices for a triangle
+
+    unsigned int VBO; //Id for vertrx buffer object
+    glGenBuffers(1, &VBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); //SAY that that when we modify and array buffer itll be that on
+
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices), vertices, GL_STATIC_DRAW);
+
     
+    //create a vertex shader thatll handle the previous shader, souce code at the tops
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); //bind source to shader
+    glCompileShader(vertexShader); //tell it to use that one
+
+    //CHECK FOR ERROR IN SHADERS----------------------
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog); std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    //vertex shader end
+
+    //fragmentsshder
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL); //bind source to shader
+    glCompileShader(fragmentShader); //tell it to use that one
+
+    //CHECK FOR ERROR IN SHADERS----------------------
+    success = NULL;
+    char infoLog2[512];
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+    if(!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog2); std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    //fragmentshader end
+
+    // triangle end
     while(!glfwWindowShouldClose(window)) //Render LOOP
     {
         //Check key presses
@@ -52,6 +123,9 @@ int main(void)
     return 0;
 }
 
+
+
+//Function definitions --------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0,0,width, height);
